6981259aa2625d10519c22536f4c382fd05ac69c
drumnistnakano <30113636+drumnistnakano@users.noreply.github.com>
2024-11-20 10:59:32 +0900

    viteによるReact導入

--- chat-app/src/main.tsx
+++ chat-app/src/main.tsx
@@ -2,16 +2,19 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Amplify } from "aws-amplify";
 import "@aws-amplify/ui-react/styles.css";
-import outputs from "../amplify_outputs.json";
+import outputs from "../../amplify_outputs.json";
 import { Authenticator } from "@aws-amplify/ui-react";
 import { App } from "./App";
 
 Amplify.configure(outputs);
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
-  </StrictMode>
-);
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <Authenticator>
+        <App />
+      </Authenticator>
+    </StrictMode>
+  );
+}
